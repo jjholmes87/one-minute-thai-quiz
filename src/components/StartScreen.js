70240bc1630e20b0b0a8184393ba61c54f84a28e
JHolmes87 <64210391+JHolmes87@users.noreply.github.com>
2024-08-08 20:29:13 +0700

    Redone start screen

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,14 +1,18 @@
-// src/components/StartScreen.js
 import React from 'react';
 import '../App.css'; // Import the stylesheet
 
 const StartScreen = ({ onStart }) => {
   return (
     <div className="start-screen">
-      <h1>One Minute Thai Quiz</h1>
+      <div className="header-container">
+        <h1>One Minute Thai Quizzes</h1>
+        <img src={`${process.env.PUBLIC_URL}/logo192.png`} alt="Logo" className="logo" />
+      </div>
+      <h2>Vocabulary</h2>
       <button className="start-button" onClick={() => onStart('thai-to-english')}>Thai ➡️ English</button>
       <button className="start-button" onClick={() => onStart('english-to-thai')}>English ➡️ Thai</button>
-      <button className="start-button" onClick={() => onStart('thai-consonant-class')}>Thai Consonant Class</button>
+      <h2>Consonants</h2>
+      <button className="start-button" onClick={() => onStart('thai-consonant-class')}>Classes</button>
     </div>
   );
 };
